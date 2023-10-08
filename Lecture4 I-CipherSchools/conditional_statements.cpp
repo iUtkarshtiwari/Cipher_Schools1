2776ab7d1cb6526cbf388da151cd3757b56473de
tanish <113004510+Tanishsingal@users.noreply.github.com>
2023-10-08 23:21:48 +0530

    Update conditional_statements.cpp

--- Lecture4 I-CipherSchools/conditional_statements.cpp
+++ Lecture4 I-CipherSchools/conditional_statements.cpp
@@ -1,9 +1,27 @@
+
 #include<iostream>
 using namespace std;
+
 int main(){
-    int a,s,d,f;
-    cin>>a>>s>>d>>f;
-    cout<<(a>s && f<d)<<endl;
-    cout<<(a<s || d<f);
+
+    int a,b,c,d;
+
+    cout<<"Input 4 integers  a,b,c and d:";
+    cout<<endl;
+    cin>>a>>b>>c>>d;
+   
+
+    // execute AND and OR statements and print out the Boolean: 
+    
+    //for AND condition:
+    cout<<(a>b && c>d);
+    
+    cout<<endl;
+    
+    //for OR condition:
+    cout<<(a>b || c>d);
+    
+    cout<<endl;
+
     return 0;
 }
