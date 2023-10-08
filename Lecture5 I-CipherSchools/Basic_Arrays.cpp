0bef83790be98c43fb8cfba774441d41a3665680
tanish <113004510+Tanishsingal@users.noreply.github.com>
2023-10-08 23:03:29 +0530

    Update Basic_Arrays.cpp

--- Lecture5 I-CipherSchools/Basic_Arrays.cpp
+++ Lecture5 I-CipherSchools/Basic_Arrays.cpp
@@ -1,13 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    int n;//this can store only 1 values
-    int a[5];//this became an array which can store 5 elements
-    a[0]=72;
-    a[1]=12;
-    a[2]=52;
-    a[3]=42;
-    a[4]=42;
-    cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4];
-    return 0;
+//without using arrays
+
+  int a,b,c,d,e;
+  cout<<"enter the values of 5 integers: "<<endl;
+  cin>>a>>b>>c>>d>>e;
+  cout<<"intergers in reverse order "<<e<<d<<c<<b<<a<<endl;
+  return 0;
 }
