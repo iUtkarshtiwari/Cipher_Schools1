b4fb3ae3a19287f3cdd4431235f2e0473f0cd95d
tanish <113004510+Tanishsingal@users.noreply.github.com>
2023-10-08 23:05:16 +0530

    Loops_part1.cpp

--- Lecture6 I-CipherSchools/Loops_part1.cpp
+++ Lecture6 I-CipherSchools/Loops_part1.cpp
@@ -1,16 +1,12 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
 int main(){
-    //printning natural numbers
-    cout<<1<<endl;
-    cout<<2<<endl;
-    cout<<3<<endl;
-    //to remove this repetition we will use loops
-    //For Loop
     int n;
+    cout<<"enter the no. upto u want to print "<<endl;
     cin>>n;
-    for(int i=1; i<=n; i++){
-        cout<<i<<endl;
+    for(int i=0; i<=n ; i++){
+        cout<<i<<" ";
     }
-    return 0;
+  return 0;
 }
