8ff9d208fedd34b09c4c7d37bf840968da224398
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-18 20:40:41 +0530

    Hi

--- forloop.java
+++ forloop.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class forloop {
+    
     public static void main(String[] args){
-    for(int i=0;i<5;i++){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    for(int i=0;i<n;i++){
         System.out.println("Hello");
     }
 }
